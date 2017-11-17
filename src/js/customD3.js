91443e29d31394e003a915a3b03f6b5312b372b0
Michela Zucca <michela.zucca@heig-vd.ch>
2017-11-17 11:53:39 +0100

    customD3 format class ok, ajout du gitignore et de la dépendance eslint

--- src/js/customD3.js
+++ src/js/customD3.js
@@ -1,47 +1,18 @@
-const div = d3.select('body').append('div')
-.attr('class', 'tooltip')
-.style('opacity', 0.2);
 
-function mouseover() {
-div.transition()
-  .duration(500)
-  .style('opacity', 0.9);
-}
-function mouseout() {
-div.transition()
-  .duration(500)
-  .style('opacity',0.2);
-}
 
 const texteCow = 'Je suis le texte de la vache youpiiii';
-const texteBeef = 'Je suis le texte de la vache youpiiii';
-const textePig = 'Je suis le texte de la vache youpiiii';
-
-function mousemove() {
-div
-    .html(`<span>${texteCow}</span>`)
-    .style('left', `${d3.event.pageX - 34}px`)
-    .style('top', `${d3.event.pageY - 12}px`);
-}
-function mousemove2() {
-div
-   // .html(`<span>${texteBeef}</span>`)
-   .html(`<span>Lalalal</span>`)
-   .style('left', `${d3.event.pageX - 34}px`)
-   .style('top', `${d3.event.pageY - 12}px`);
-}
-function mousemove3() {
-div
-    // .html(`<span>${texteBeef}</span>`)
-    .html(`<span>hihihihi</span>`)
-    .style('left', `${d3.event.pageX - 34}px`)
-    .style('top', `${d3.event.pageY - 12}px`);
-}
+const textePig = 'Je suis le texte de la cochonnnn youpiiii';
+const textePoultry= 'Je suis le texte du pouleeeet youpiiii';
 
 class ImgInteractive {
-    constructor(idElement, imageSrc, move, msg) {
+    constructor(idElement, imageSrc, msg) {
       this.idElement = idElement;
       this.imageSrc = imageSrc;
+
+      this.div = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0);
+      
       
       this.svgImg = d3.select(idElement).append('svg')
       .attr({
@@ -53,13 +24,32 @@ class ImgInteractive {
       this.svgImg.append('svg:image')
       .attr('width', 200)
       .attr('height', 200)
-      .attr('xlink:href', imageSrc)
-      .on('mouseover', mouseover)
-      .on('mousemove', move)
-      .on('mouseout', mouseout);
+      .attr('xlink:href', this.imageSrc)
+      .on('mouseover', this.mouseover.bind(this))
+      .on('mousemove', () => {
+        this.mousemove(msg);
+      })
+      .on('mouseout',this.mouseout.bind(this));
     }    
-  }
+
+    mousemove(msg) {
+      this.div
+        .html(`<span>${msg}</span>`)
+        .style('left', `${d3.event.pageX - 34}px`)
+        .style('top', `${d3.event.pageY - 12}px`);
+    }
+    mouseover() {
+      this.div.transition()
+        .duration(500)
+        .style('opacity', 0.9);
+    }
+     mouseout() {
+      this.div.transition()
+        .duration(500)
+        .style('opacity',0);
+   }
+ }
   
-  const image = new ImgInteractive('#tableInteractive', 'img/tableIconeVache.jpg', mousemove);
-  const image2 = new ImgInteractive('#tableInteractive', 'img/tableIconeCochon.jpg',mousemove2);
-  const image3 = new ImgInteractive('#tableInteractive', 'img/tableIconePoulet.png',mousemove3);
+  const image = new ImgInteractive('#tableInteractive', 'img/tableIconeVache.jpg', texteCow);
+  const image2 = new ImgInteractive('#tableInteractive', 'img/tableIconeCochon.jpg',textePig);
+  const image3 = new ImgInteractive('#tableInteractive', 'img/tableIconePoulet.png',textePoultry);
