6db25c426ddb923863d0525f49e698a84a6419f8
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-11-29 10:32:45 +0100

    New structure of images

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,17 +1,6 @@
 let map;
-
-const beefImage = new InteractiveImg('interactive-img-beef', 'svg/beef.svg');
-const pigImage = new InteractiveImg('interactive-img-pig', 'svg/pig.svg');
-const chickenImage = new InteractiveImg('interactive-img-chicken', 'svg/chicken.svg');
-
-const vegetablesImage = new InteractiveImg('interactive-img-vegetables', 'svg/vegetables.svg');
-const cerealsImage = new InteractiveImg('interactive-img-cereals', 'svg/cereals.svg');
-const beansImage = new InteractiveImg('interactive-img-beans', 'svg/beans.svg');
-
-const oneHamburgerMeat = new InteractiveImg('interactive-img-one-hamburger-meat', 'svg/earth.svg');
-const oneHamburgerVegetarian = new InteractiveImg('interactive-img-one-hamburger-vegetarian', 'svg/earth.svg');
-const allHamburgersMeat = new InteractiveImg('interactive-img-all-hamburgers-meat', 'svg/earth.svg');
-const allHamburgersVegetarian = new InteractiveImg('interactive-img-all-hamburgers-vegetarian', 'svg/earth.svg');
+let foodStyle;
+let planetsStyle;
 
 $(() => {
   $.getJSON('data/countries.geo.json')
@@ -19,77 +8,74 @@ $(() => {
       map = new WorldMap('map', data);
     });
 
-  $.getJSON('data/beef.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      beefImage.addDescription(description);
-      beefImage.enableInteraction();
-      beefImage.enableSound('https://upload.wikimedia.org/wikipedia/commons/4/48/Mudchute_cow_1.ogg');
-    });
+  $.when(
+    $.getJSON('data/food-style.json', (data) => {
+      foodStyle = data;
+    }),
+    $.getJSON('data/planets-style.json', (data) => {
+      planetsStyle = data;
+    }),
+  ).then(() => {
+    $.getJSON('data/beef.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-beef', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/pig.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      pigImage.addDescription(description);
-      pigImage.enableInteraction();
-      pigImage.enableSound('https://upload.wikimedia.org/wikipedia/commons/4/4a/Mudchute_pig_2.ogg');
-    });
+    $.getJSON('data/pig.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-pig', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/chicken.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      chickenImage.addDescription(description);
-      chickenImage.enableInteraction();
-    });
+    $.getJSON('data/chicken.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-chicken', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/vegetables.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      vegetablesImage.addDescription(description);
-      vegetablesImage.enableInteraction();
-    });
+    $.getJSON('data/beans.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-beans', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/cereals.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      cerealsImage.addDescription(description);
-      cerealsImage.enableInteraction();
-    });
+    $.getJSON('data/cereals.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-cereals', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/beans.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      beansImage.addDescription(description);
-      beansImage.enableInteraction();
-    });
+    $.getJSON('data/vegetables.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-vegetables', data, foodStyle);
+        image.enableFullInteractivity();
+      });
 
-  $.getJSON('data/one-hamburger-meat.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      oneHamburgerMeat.addDescription(description);
-      oneHamburgerMeat.enableInteraction();
-    });
-
-  $.getJSON('data/one-hamburger-vegetarian.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      oneHamburgerVegetarian.addDescription(description);
-      oneHamburgerVegetarian.enableInteraction();
-    });
-
-  $.getJSON('data/all-hamburgers-meat.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      allHamburgersMeat.addDescription(description);
-      allHamburgersMeat.enableInteraction();
-    });
-
-  $.getJSON('data/all-hamburgers-vegetarian.json')
-    .done((data) => {
-      const description = new ImgDescription(data);
-      allHamburgersVegetarian.addDescription(description);
-      allHamburgersVegetarian.enableInteraction();
-    });
+    $.getJSON('data/one-hamburger-meat.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-one-hamburger-meat', data, planetsStyle);
+        image.enableFullInteractivity();
+      });
+  
+    $.getJSON('data/one-hamburger-vegetarian.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-one-hamburger-vegetarian', data, planetsStyle);
+        image.enableFullInteractivity();
+      });
+  
+    $.getJSON('data/all-hamburgers-meat.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-all-hamburgers-meat', data, planetsStyle);
+        image.enableFullInteractivity();
+      });
+  
+    $.getJSON('data/all-hamburgers-vegetarian.json')
+      .done((data) => {
+        const image = new InteractiveImg('interactive-img-all-hamburgers-vegetarian', data, planetsStyle);
+        image.enableFullInteractivity();
+      });
+  });
 });
 
 function refreshMap() {
